42c7856a9c36b34c5805df8c0b664c7829ebb58c
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-04-12 20:35:01 +0100

    Add files via upload

--- awk.sh
+++ awk.sh
@@ -1,2 +1,9 @@
 printf "\ec\e[44;37m\n"
-awk 'BEGIN{for(i=1;i<=100;i++) print i}'
+list=$(awk 'BEGIN {for (i=1; i<=100; i++) print "hello" i "world" i}' )
+echo -e "$list" | while read line; do
+    l1=$(echo "${line/hello/hi}")
+    echo "${l1//world/there}"
+done
+
+
+
