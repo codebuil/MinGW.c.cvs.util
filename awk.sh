1c211e9d55fdc4aaa9a663f165ec27cf702f599c
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-04-20 18:23:11 +0100

    Delete awk.sh

--- awk.sh
+++
@@ -1,9 +0,0 @@
-printf "\ec\e[44;37m\n"
-files=/dev/stdout
-list=$(awk 'BEGIN { srand(); for(i=1; i<=100; i++) { printf("%d,%d\n",i, int(50*rand())+1) } }')
-echo -e "$list" | while read line; do    
-    echo "$line" > "$files"
-done
-
-
-
